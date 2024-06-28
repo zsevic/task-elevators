c7adecfeca25aee0df89653a474defc6915c9eb2
zsevic <zsevic95@gmail.com>
2024-06-28 17:18:47 +0200

    Add comments

--- elevator.js
+++ elevator.js
@@ -10,18 +10,22 @@ class Elevator {
   }
 
   move() {
+    // Filter requests where the current floor matches the origin and the origin has not been visited
     const originRequests = this.requests.filter(
       (request) =>
         request.origin === this.currentFloor && !request.originVisited
     );
+    // Mark these origin requests as visited
     if (originRequests.length > 0) {
       originRequests.forEach((request) => (request.originVisited = true));
     }
 
+    // Filter requests where the current floor matches the destination and the origin has been visited
     const destinationRequests = this.requests.filter(
       (request) =>
         request.destination === this.currentFloor && request.originVisited
     );
+    // Remove requests that have reached their destination
     if (destinationRequests.length > 0) {
       this.requests = this.requests.filter(
         (request) =>
@@ -38,6 +42,8 @@ class Elevator {
       const targetFloor = !nextRequest.originVisited
         ? nextRequest.origin
         : nextRequest.destination;
+
+      // Set the direction based on the target floor
       if (this.currentFloor < targetFloor) {
         this.direction = DIRECTION.up;
       } else if (this.currentFloor > targetFloor) {
@@ -58,11 +64,14 @@ class Elevator {
     if (this.status !== STATUS.idle) return;
 
     this.status = STATUS.moving;
+    // Determine the direction based on the current floor and the origin of the new request
     if (this.currentFloor < origin) {
       this.direction = DIRECTION.up;
     } else if (this.currentFloor > origin) {
       this.direction = DIRECTION.down;
-    } else if (this.currentFloor < destination) {
+    }
+    // If the elevator is already at the origin floor, determine the direction based on the destination
+    else if (this.currentFloor < destination) {
       this.direction = DIRECTION.up;
     } else if (this.currentFloor > destination) {
       this.direction = DIRECTION.down;
